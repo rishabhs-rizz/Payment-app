2891f3c31afc495358c107a86b38ae27a96e04a2
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-02-25 13:51:35 +0530

    init

---
+++ backend/db.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://rishabhshukla2924:rish7985@cluster0.bmmtb.mongodb.net/payments-app?retryWrites=true&w=majority")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+// Create a model from the schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User
+};
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const mainRouter = require("./routes/routes");
 
 
+app.use("api/v1/", mainRouter);
+
+app.listen(3000);
