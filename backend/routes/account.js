659f28269b3d0d865aca24edacf62e4e1524ff72
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-03-04 12:28:01 +0530

    balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const auth_middleware = require("../middleware");
+const { Account } = require("../db");
 const router = express.Router();
 
+router.get("/balance", auth_middleware, async (req , res) => {
+    const account = await Account.findOne({
+        userId: req.userId
+    })
+
+    res.json({
+        balance: account.balance
+    })
+})
 module.exports = router;
