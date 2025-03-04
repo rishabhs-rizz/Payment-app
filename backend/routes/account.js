691b9fc52f1c9666ca7fd3f01bcb3c5903bb7c3b
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-03-04 12:54:17 +0530

    added transactions in db

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const auth_middleware = require("../middleware");
 const { Account } = require("../db");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
 
 router.get("/balance", auth_middleware, async (req , res) => {
@@ -11,5 +12,44 @@ router.get("/balance", auth_middleware, async (req , res) => {
     res.json({
         balance: account.balance
     })
+})
+
+router.post("/transfer", auth_middleware, async (req , res) => {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    const { amount , to } = req.body;
+
+    const account = await Account.findOne({
+        userId: req.userId
+    }).session(session);
+
+    if(!account || account.balance < amount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Insufficient balance"
+        });
+    }
+
+    const toAccount = await Account.findOne({
+        userId: to
+    }).session(session);
+
+    if(!toAccount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Invalid account"
+        });
+    }
+
+    await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount}}).session(session);
+
+    await Account.updateOne({ userId: to }, { $inc: { balance: amount }}).session(session);
+
+    await session.commitTransaction();
+    res.json({
+        message: "Transfer successful"
+    });
+
 })
 module.exports = router;
