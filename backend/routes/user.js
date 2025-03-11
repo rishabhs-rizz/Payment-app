941fb5d512349f29b391431972b0758c65653e0d
Rishabh shukla <rishabhshukla2924@gmail.com>
2025-03-11 16:48:53 +0530

    updated be

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -41,26 +41,47 @@ try{
             firstName: req.body.firstName,
             lastName: req.body.lastName
         })
-        const UserId = user._id
-    
-        console.log(Account)
+        const UserId = user._id;
     
         await Account.create({
             userId: UserId,
             balance: 1 + Math.random() * 10000
         })
-    
         const token = jwt.sign({
             UserId: UserId
         }, JWT_SECRET);
     
         res.json({
             message: "user successfully registered",
-            token: token
+            token: token,
         })
     })
 }catch(e){console.error(e);}
 
+try{
+    
+    router.post("/signin", async (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+    
+        const existingUser = await User.findOne({
+            username: username
+        })
+    
+        if(existingUser){
+            
+            res.json({
+                message: "you are successfully logged in",
+            })
+        }else {
+            res.json({
+                message: "incorrect username or password"
+            })
+        }
+    })
+}catch(e){console.error(e);}
+
+
 try {
     const updatedBody = zod.object({
 
@@ -90,47 +111,15 @@ try {
 }
 
 try {
-    const updatedBody = zod.object({
-
-        password: zod.string().optional(),
-        firstName: zod.string().optional(),
-        lastName: zod.string().optional()
-    })
-    
-    router.put("/updation", auth_middleware, async (req, res) => {
-        const { success } = updatedBody.safeParse(req.body);
-        if(!success){ // stop if given data is not in required format
-            return res.json({
-                message: "error while updating information"
-            })
-        }
-    
-        await User.updateOne(req.body, {
-            id: req.UserId
-        })
-    
-        res.json({
-            message: "updated successfully"
-        })
-    })
-} catch (e) {
-    console.error(e);
-}
-
-try {
-    router.get("/bulk",auth_middleware, async (req , res) => {
+    router.get("/bulk", async (req , res) => {
         console.log("Route Hit")
         const filter = req.query.filter || "";
     
         const Users = await User.find({
-            $or: [{
-                firstName: {
-                    $regex: filter
-                },
-                lastName: {
-                    $regex: filter
-                }
-            }]
+            $or: [
+                { firstName: { $regex: filter, $options: "i" } },
+                { lastName: { $regex: filter, $options: "i" } }
+            ]
         })
     
         res.json({
@@ -139,7 +128,7 @@ try {
                 firstName: user.firstName,
                 lastName: user.lastName,
                 _id: user._id
-            }))
+            }) || [])
         })
     })    
 } catch (e) {
